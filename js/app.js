fc23f7055272ecf5a0f9818bbad7697c7f84c86f
Edison Lati Jr. <junior.lati@gmail.com>
2025-10-17 09:21:25 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
-let app = angular.module("GroceryApp", []);
+/**
+ * Created by Thomas on 5/28/2015.
+ */
+let app = angular.module('groceryListApp', ["ngRoute"]);
+
+app.config(["$routeProvider", function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "views/groceryList.html",
+            controller: "GroceryListItemsController"
+        })
+        .when("/addItem", {
+            templateUrl: "views/addItem.html",
+            controller: "GroceryListItemsController"
+        })
+        .when("/addItem/:id", {
+            templateUrl: "views/addItem.html",
+            controller: "GroceryListItemsController"
+        })
+        .otherwise({
+            redirectTo: "/",
+        })
+}]);
+
+app.controller("HomeController", ["$scope", function($scope) {
+    $scope.appTitle = "Grocery List";
+}]);
+
+app.controller("GroceryListItemsController", ["$scope", "$routeParams", function($scope, $routeParams){
+
+    $scope.groceryItems = [
+        {completed: true, itemName: 'milk', date: '2014-10-00'},
+        {completed: true, itemName: 'cookies', date: '2014-10-01'},
+        {completed: true, itemName: 'ice cream', date: '2014-10-02'},
+        {completed: true, itemName: 'potatoes', date: '2014-10-02'},
+        {completed: true, itemName: 'cereal', date: '2014-10-03'},
+        {completed: true, itemName: 'bread', date: '2014-10-03'},
+        {completed: true, itemName: 'eggs', date: '2014-10-04'},
+        {completed: true, itemName: 'tortillas', date: '2014-10-04'}
+    ]
+
+    $scope.rp = "Route Parameter Values: " + $routeParams.id;
+}]);
