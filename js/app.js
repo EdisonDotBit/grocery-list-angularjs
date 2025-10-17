b0b1cd5233c3aba758936acd7ebe8a1f180617f1
Edison Lati Jr. <junior.lati@gmail.com>
2025-10-17 11:19:42 +0800

    Update index.html
    Added  <script src="lib/underscore-min.js"></script> dependency

--- js/app.js
+++ js/app.js
@@ -22,22 +22,53 @@ app.config(["$routeProvider", function($routeProvider) {
         })
 }]);
 
+app.service("GroceryService", function() {
+    let groceryService = {};
+
+    groceryService.groceryItems = [
+        {id: 1, completed: true, itemName: 'milk', date: '2014-10-00'},
+        {id: 2, completed: true, itemName: 'cookies', date: '2014-10-01'},
+        {id: 3, completed: true, itemName: 'ice cream', date: '2014-10-02'},
+        {id: 4, completed: true, itemName: 'potatoes', date: '2014-10-02'},
+        {id: 5, completed: true, itemName: 'cereal', date: '2014-10-03'},
+        {id: 6, completed: true, itemName: 'bread', date: '2014-10-03'},
+        {id: 7, completed: true, itemName: 'eggs', date: '2014-10-04'},
+        {id: 8, completed: true, itemName: 'tortillas', date: '2014-10-04'}
+    ];
+    
+    groceryService.getNewId =  function() {
+        if(groceryService.newId) {
+            groceryService.newId++;
+            return groceryService.newId;
+        } else {
+            let maxId = _.max(groceryService.groceryItems, function(entry) {
+                return entry.id
+            })
+            groceryService.newId = maxId.id + 1;
+            return groceryService.newId;
+        }
+    } 
+
+    groceryService.save = function(entry) {
+        entry.id = groceryService.getNewId();
+        groceryService.groceryItems.push(entry);
+    }
+
+    return groceryService;
+});
+
 app.controller("HomeController", ["$scope", function($scope) {
     $scope.appTitle = "Grocery List";
 }]);
 
-app.controller("GroceryListItemsController", ["$scope", "$routeParams", function($scope, $routeParams){
+app.controller("GroceryListItemsController", ["$scope", "$routeParams", "$location", "GroceryService", function($scope, $routeParams, $location, GroceryService ){
 
-    $scope.groceryItems = [
-        {completed: true, itemName: 'milk', date: '2014-10-00'},
-        {completed: true, itemName: 'cookies', date: '2014-10-01'},
-        {completed: true, itemName: 'ice cream', date: '2014-10-02'},
-        {completed: true, itemName: 'potatoes', date: '2014-10-02'},
-        {completed: true, itemName: 'cereal', date: '2014-10-03'},
-        {completed: true, itemName: 'bread', date: '2014-10-03'},
-        {completed: true, itemName: 'eggs', date: '2014-10-04'},
-        {completed: true, itemName: 'tortillas', date: '2014-10-04'}
-    ]
+    $scope.groceryItems = GroceryService.groceryItems;
 
-    $scope.rp = "Route Parameter Values: " + $routeParams.id;
+    $scope.groceryItem = { id: 7, completed: true, itemName: 'cheese', date: new Date() };
+
+    $scope.save = function() {
+        GroceryService.save($scope.groceryItem);
+        $location.path("/");
+    }
 }]);
